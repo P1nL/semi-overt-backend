9d7d9565d27cf927149ff810ef1e0e2558187dfb
P1nL <3382727671@qq.com>
2026-04-12 17:52:31 +0800

    清理缓存示例文件
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- .cache/ShowHome.java
+++
@@ -1,6 +0,0 @@
-public class ShowHome {
-  public static void main(String[] args) {
-    System.out.println("user.home=" + System.getProperty("user.home"));
-    System.out.println("user.name=" + System.getProperty("user.name"));
-  }
-}
